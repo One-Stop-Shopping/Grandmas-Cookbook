39536e43e4a8ea9b3c4a131f6f707c37d44b761f
Yi Sun <timyisun@gmail.com>
2023-04-05 19:25:35 -0500

    Added one example web-scraping to Foodwork.com for ingredient.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 
+// Fetch from www.epicurious.com
 const basicMuffins =
   'https://www.epicurious.com/recipes/food/views/basic-muffins-40037';
 const coconutMuffins =
@@ -13,7 +14,31 @@ fetch(coconutMuffins)
     const ingredientList = $('div[data-testid="IngredientList"]').children(
       'div'
     );
+    console.log('Coconut Muffins \n');
     ingredientList.children().each((i, el) => {
       console.log($(el).text());
     });
+    console.log('\n');
   });
+
+// Fetch from www.foodnetwork.com
+const shrimpScampi =
+  'https://www.foodnetwork.com/recipes/ree-drummond/shrimp-scampi-2580746';
+
+fetch(shrimpScampi)
+  .then((res) => res.text())
+  .then((html) => {
+    const $ = cheerio.load(html);
+    const ingredientList = $('.o-Ingredients__a-Ingredient--CheckboxLabel');
+    console.log('Shrimp Scampi \n');
+    ingredientList.each((i, el) => {
+      if (i) {
+        console.log($(el).text());
+      }
+    });
+    console.log('\n');
+  });
+
+/*
+ * To-Do: get the recipe name
+ */
