075490ac0a9985654e34003058d8b2751031d91b
Djaworski <jaworskidaniel1@gmail.com>
2023-04-05 21:43:30 -0500

    Successfully rendering wrapper react component in index.html

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// eslint-disable-next-line no-unused-vars
+function App(props) {
+
+    return (
+        <div>
+            <h1>Hello!!!</h1>
+        </div>
+    );
+};
+
+export default App;
