cd699ce22af5d385bc0d4d4178beb436b5cdcae3
Djaworski1 <119364737+Djaworski1@users.noreply.github.com>
2023-04-08 10:45:47 -0500

    Revert "Daniel/card grid"

--- server/routes/recipeRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const scrapingController = require('../controller/scrapingController');
-
-router.get('/scrapeurl', scrapingController, (req, res, next) => {
-  res.status(200).json(res.locals);
-});
-
-module.exports = router;
