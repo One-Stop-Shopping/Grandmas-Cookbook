14ca0257fc9a5fa6ee0ec7cc74cf94a19b2188ee
Yi Sun <timyisun@gmail.com>
2023-04-08 09:02:13 -0500

    Added comments and notes for the databaseModels file.

--- server/models/databaseModels.js
+++ server/models/databaseModels.js
@@ -8,18 +8,38 @@ const pool = new Pool({
 });
 
 /*
- TO-DO: Add notes about the database (schema, etc.)
-  CREATE TABLE recipe (
-    url VARCHAR(255) PRIMARY KEY,
-    title VARCHAR(255),
+It is a good practice to have id in the table, to provide a stable reference point (won't change if any data of the row is updated) for other tables.  
+
+  CREATE TABLE recipes (
+    id SERIAL PRIMARY KEY, 
+    url VARCHAR(255) UNIQUE NOT NULL,
+    title VARCHAR(255) NOT NULL,
     ingredientList JSON,
     directions JSON,
-    image BYTEA
+    image_path VARCHAR(255),
+  )
+  
+  Store the image in file system and store the path to image file in the image_path column of recipes table.
+    OR
+  Store the recipe's image (binary data itself) in separate table to keep the recipes table lean and efficient pg_read_binary_file('/path-to-image/')
+
+  CREATE TABLE recipeImages (
+    id SERIAL PRIMARY KEY,
+    image BYTEA,
+    recipe_id VARCHAR(255) REFERENCES recipes (id),
   )
 
-  CREATE TABLE user (
-    username VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(50),
+  CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+  )
+  
+  Join table for the many to many relation between recipes and users 
+  CREATE TABLE users_recipes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users (id),
+    recipe_id INTEGER REFERENCES recipes (id),
   )
  */
 
